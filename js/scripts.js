c509c005881ab644091b66abda078ff213e253a1
Roygicheru <gicheru.k.roy@gmail.com>
2019-02-24 19:36:00 +0300

    add first question in index.html

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,28 @@ var count = 0;
 function check() {
   var qone = document.quizy.qone.value;
   var qtwo = document.quizy.qtwo.value;
-
+  var qthree = document.quizy.qthree.value;
+  var qfour = document.quizy.qfour.value;
+  var qfive = document.quizy.qfive.value;
 
   if(qone == "1") {
-    count += 50;
+    count += 20;
   }
 
   if(qtwo == "2") {
-    count += 50;
+    count += 20;
+  }
+
+  if(qthree == "2") {
+    count += 20;
+  }
+
+  if(qfour == "1") {
+    count += 20;
+  }
+
+  if(qfive == "3") {
+    count += 20;
   }
   return count
 }
